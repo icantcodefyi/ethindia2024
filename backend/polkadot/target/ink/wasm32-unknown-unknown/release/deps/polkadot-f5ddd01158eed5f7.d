74d6cde1b86b10a3ef084261b2b08f13140afcf8
Aniruddh Dubge <aniruddhdubge@Aniruddhs-MacBook-Air.local>
2024-12-07 12:12:09 +0530

    update

--- backend/polkadot/target/ink/wasm32-unknown-unknown/release/deps/polkadot-f5ddd01158eed5f7.d
+++
@@ -1,5 +0,0 @@
-/Users/aniruddhdubge/icantcodefyi/hackathons/ethindia2024/backend/polkadot/target/ink/wasm32-unknown-unknown/release/deps/polkadot-f5ddd01158eed5f7.wasm: /Users/aniruddhdubge/icantcodefyi/hackathons/ethindia2024/backend/polkadot/lib.rs
-
-/Users/aniruddhdubge/icantcodefyi/hackathons/ethindia2024/backend/polkadot/target/ink/wasm32-unknown-unknown/release/deps/polkadot-f5ddd01158eed5f7.d: /Users/aniruddhdubge/icantcodefyi/hackathons/ethindia2024/backend/polkadot/lib.rs
-
-/Users/aniruddhdubge/icantcodefyi/hackathons/ethindia2024/backend/polkadot/lib.rs:
--- backend/polkadot/target/ink/wasm32-unknown-unknown/release/polkadot.d
+++
@@ -1,1 +0,0 @@
-/Users/aniruddhdubge/icantcodefyi/hackathons/ethindia2024/backend/polkadot/target/ink/wasm32-unknown-unknown/release/polkadot.wasm: /Users/aniruddhdubge/icantcodefyi/hackathons/ethindia2024/backend/polkadot/lib.rs
