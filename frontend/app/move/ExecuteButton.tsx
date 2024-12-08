d375332ce163746f349618b1f82d13b4ff2f1f63
Sudhan <sudhanplayz@gmail.com>
2024-12-08 09:24:12 +0530

    w

--- frontend/app/move/ExecuteButton.tsx
+++ frontend/app/move/ExecuteButton.tsx
@@ -6,7 +6,23 @@ import { cn } from "@/lib/utils";
 import { toast } from "@/hooks/use-toast";
 import { BlockType } from "@/constants/paths";
 import { StarKeyProvider } from "@/types/starkey";
-import { SUPRA_TOKEN_CONTRACT, createSupraTransactionData } from "@/constants/contracts";
+
+export const SUPRA_TOKEN_CONTRACT = {
+  address: "0x5c81634add84286cd674e62c1e3e9d7576b3e5cce5f41508aab836a629c0d0a",
+  module: "MyToken",
+};
+
+export const createSupraTransactionData = (functionName: string, args: any[]) => {
+  return {
+    action: "createRawTransactionData",
+    params: {
+      contractAddress: SUPRA_TOKEN_CONTRACT.address,
+      module: SUPRA_TOKEN_CONTRACT.module,
+      function: functionName,
+      args
+    }
+  };
+};
 
 interface ExecuteButtonProps {
   blocks: BlockType[];
